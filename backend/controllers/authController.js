d38d258d74af505fbe3f6cc5c2a212911f3e56bf
Ahmad Ma'ruf <ahmadmaruf2701@gmail.com>
2025-07-17 18:48:37 +0700

    fix: generate token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { pool } = require('../config/database');
 
 // Generate JWT token
-const generateToken = (adminId, role) => {
+const generateToken = (userId, role) => {
   return jwt.sign(
     { userId, role },
     process.env.JWT_SECRET || 'your-secret-key',
